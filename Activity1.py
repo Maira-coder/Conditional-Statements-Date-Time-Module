2764ff1aa9483947882c24d79e757464731100ad
Maira-coder <mairanoor145@gmail.com>
2025-08-12 15:53:44 +0100

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,8 @@
+num=int(input ("Put a number"))
+if num %2==0:
+    print("Number is even")
+else: 
+    print("Number is odd")
+
+
+
---
+++ Activity4.py
@@ -0,0 +1,5 @@
+import datetime,calendar
+
+num=datetime.datetime.now()
+print("The time is ",num)
+print("\n", calendar.calendar(2025))
