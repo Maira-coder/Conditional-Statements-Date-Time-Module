9e5f9a7cd587cfb059ec371f19712bd32c91966a
Maira-coder <mairanoor145@gmail.com>
2025-08-19 17:27:35 +0100

    Add files via upload

--- Homework.py
+++ Homework.py
@@ -1,7 +1,7 @@
 num=int(input("Enter the amount of days you attended school: "))
 num2=int(input("Enter the amount of days you were absent: "))
 
-if num>num2%75:
+if num2<num%75:
     print("You are eligible to the attend the exams.")
 else:
     print("You are not eligible to attend the exam.")
